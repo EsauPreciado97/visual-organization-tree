1df0444ee006f62779fb414ab055bbc3e513de4d
esaupreciado97 <esauplm@gmail.com>
2021-07-13 13:15:41 -0500

    Visual tree

--- src/containers/dashboard/Dashboard.js
+++ src/containers/dashboard/Dashboard.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './Dashboard.css';
-import Header from '../../components/header/Header'
+import Header from '../../components/header/Header';
+import Sidebar from '../../components/sidebar/Sidebar';
+import Tree from '../../components/tree/Tree';
+import Grid from '@material-ui/core/Grid';
+
 
 function Dashboard() {
   return (
-    <Header />
+    <div className={"dashboard-container"}>
+      <Header />
+      <Grid className={"dashboard-grid"}container spacing={0}>
+        <Grid item xs={2}>
+          <Sidebar />
+        </Grid>
+        <Grid item xs={10}>
+          <Tree />
+        </Grid>
+      </Grid>
+    </div>
   );
 }
 
